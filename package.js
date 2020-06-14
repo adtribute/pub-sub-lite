6ba2de6b2217e251c31c1066b303baf50194ca5d
Phuc Nguyen <contact@phucnguyen.info>
2020-06-15 01:39:15 +0700

    Publish the package under the maestroqadev account

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  name: 'npvn:pub-sub-lite',
-  version: '1.0.0',
-  summary: 'Lighter (Method-based) pub/sub for Meteor',
-  git: 'https://github.com/adtribute/pub-sub-lite',
-  documentation: 'README.md',
+  name: "maestroqadev:pub-sub-lite",
+  version: "1.0.0",
+  summary: "Lighter (Method-based) pub/sub for Meteor",
+  git: "https://github.com/adtribute/pub-sub-lite",
+  documentation: "README.md",
 });
 
 Package.onUse(function (api) {
-  api.use(['ecmascript@0.14.3', 'check@1.3.1']);
-  api.mainModule('client.js', 'client');
-  api.mainModule('server.js', 'server');
+  api.use(["ecmascript@0.14.3", "check@1.3.1"]);
+  api.mainModule("client.js", "client");
+  api.mainModule("server.js", "server");
 });
 
 Npm.depends({
-  lodash: '4.17.15',
-  'object-hash': '2.0.3',
+  lodash: "4.17.15",
+  "object-hash": "2.0.3",
 });
