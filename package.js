ad8ee22f56d9c0882eaad93e3524ac865cb93fc5
Phuc Nguyen <contact@phucnguyen.info>
2020-06-15 01:55:45 +0700

    Basic tests
    More advanced tests to be added in the future:
      - Simulated subscription handles.
      - Caching effects on `Meteor.subscribeLite`, `Meteor.callEnhanced` and `Meteor.applyEnhanced`.
      - `cacheMethodResult`, `cacheMethodResultInMinimongo` and `mergeDataIntoMinimongo` helpers.
      - The interaction between `publishLite`, `publishCompositeLite` and `subscribeLite`.
      - The interaction between `Meteor.methodsEnhanced`, `Meteor.callEnhanced` and `Meteor.applyEnhanced`.
      - Lite composite publications.
      - Enhanced `ValidatedMethod`.
      - Method call data caching and syncing with Minimongo.
      - Mutation update messages emitting.

--- package.js
+++ package.js
@@ -10,9 +10,40 @@ Package.onUse(function (api) {
   api.use(["ecmascript@0.14.3", "check@1.3.1"]);
   api.mainModule("client.js", "client");
   api.mainModule("server.js", "server");
+
+  Npm.depends({
+    lodash: "4.17.15",
+    "object-hash": "2.0.3",
+  });
 });
 
-Npm.depends({
-  lodash: "4.17.15",
-  "object-hash": "2.0.3",
+Package.onTest(function (api) {
+  api.use(["ecmascript", "check", "mongo", "meteortesting:mocha"]);
+  api.use(["jquery", "blaze-html-templates"], "client");
+  api.use("maestroqadev:pub-sub-lite");
+
+  Npm.depends({
+    lodash: "4.17.15",
+    "object-hash": "2.0.3",
+    chai: "4.2.0",
+    "babel-plugin-rewire-exports": "2.2.0",
+  });
+
+  api.addFiles(
+    [
+      "./lib/pub-sub-lite/tests/pub-sub-lite-client.tests.js",
+      "./lib/blaze/tests/blaze-template-subscribe-lite.tests.js",
+      "./lib/caching/tests/subs-cache.tests.js",
+      "./lib/caching/tests/method-calls-cache.tests.js",
+      "./lib/enhanced-methods/tests/enhanced-methods-data-handlers-client.tests.js",
+    ],
+    "client"
+  );
+  api.addFiles(
+    [
+      "./lib/pub-sub-lite/tests/pub-sub-lite-server.tests.js",
+      "./lib/enhanced-methods/tests/enhanced-methods-data-handlers-server.tests.js",
+    ],
+    "server"
+  );
 });
